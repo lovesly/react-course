4fb02ee17a8b910d5b30f9ca70de282246387e27
Zihan Zhang <zzhlovesly@outlook.com>
2018-07-31 13:56:07 +0800

    add extract-text-webpack-plugin, modify the css/sass loader

--- budget/webpack.config.js
+++ budget/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports = (env) => {
     const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('style.css');
     return {
         entry: './src/app.js',
         output: {
@@ -12,20 +15,31 @@ module.exports = (env) => {
             rules: [{
                 loader: 'babel-loader',
                 test: /\.js$/,
-                exclude: /node_modules/
+                exclude: /node_modules/,
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: 'true'
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: 'true'
+                        }
+                    }]
+                }),
             }]
         }, 
-        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
         }
     };
 };
